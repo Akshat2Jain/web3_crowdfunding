6b373cfa0159cfed471838f4ac37e0842d3f2ea1
Akshatjain <akshatj@cabin4j.com>
2023-01-20 14:24:19 +0530

    navbar new

--- project/client/src/components/Navbar.jsx
+++ project/client/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
     const [isActive, setIsActive] = useState('dashboard');
     const [toggleDrawer, setToggleDrawer] = useState(false);
   return (
-    <div>Navbar</div>
+    <div>New Navbar</div>
   )
 }
 
